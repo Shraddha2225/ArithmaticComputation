f797380d0426308168f76cb1f8c7e35908e3666e
shraddha <shraddhat22m@gmail.com>
2020-01-22 19:00:47 +0530

    ADDED: Taking input from user

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -x
 echo "Welcome to Sorting Arithmetic Computation"
-
+read -p  "Enter three numbers from user" a  b  c
+echo "value of a: $a"
+echo "value of b: $b"
+echo "value of c: $c"
