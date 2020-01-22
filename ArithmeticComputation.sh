13576e83d4403a57de90a2cea3cf99d4147ed520
shraddha <shraddhat22m@gmail.com>
2020-01-22 18:43:16 +0530

    ADDED: Welcome Arithmetic Operation

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -1,9 +1,3 @@
 #!/bin/bash -x
-read -p "Enter First Number"  a
-read -p "Enter Second Number" b
-read -p "Enter Third Number" c
-
-echo "value of a:$a"
-echo "value of b:$b"
-echo "value of c:$c"
+echo "WElcome to Sorting Arithmetic Computation"
 
