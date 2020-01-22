97048b919418b2e6ad7377f774ac189cd660a332
shraddha <shraddhat22m@gmail.com>
2020-01-22 19:13:31 +0530

    ADDED: compute operation (a+b*c)

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -4,3 +4,7 @@ read -p  "Enter three numbers from user" a  b  c
 echo "value of a: $a"
 echo "value of b: $b"
 echo "value of c: $c"
+
+#compute operation
+operation1=$((a + b * c ))
+echo "first operation(a+b*c): $operation1"
