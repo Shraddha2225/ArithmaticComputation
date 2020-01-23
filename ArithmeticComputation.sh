43de67f28dca5675ecec094d44147f1c31210058
shraddha <shraddhat22m@gmail.com>
2020-01-23 11:30:07 +0530

    ADDED: Store the computation result in a dictionary

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -x
+
+declare -A  operations
 echo "Welcome to Sorting Arithmetic Computation"
 read -p  "Enter three numbers from user" a  b  c
 echo "value of a: $a"
@@ -6,11 +8,10 @@ echo "value of b: $b"
 echo "value of c: $c"
 
 #compute operation
-operation1=$((a + b * c ))
-echo "First Operation(a+b*c): $operation1"
-operation2=$((a * b + c))
-echo "Second Operation(a*b+c): $operation2"
-operation3=$( echo "scale=2; $c + $a / $b" | bc )
-echo "Third Operation(c+a/b): $operation3"
-operation4=$((a % b + c))
-echo "Fourth Operation(a%b+c): $operation4"
+operations[1]=$((a + b * c ))
+operations[2]=$((a * b + c))
+operations[3]=$( echo "scale=2; $c + $a / $b" | bc )
+operations[4]=$((a % b + c))
+
+echo "key : ${!operations[@]} "
+echo "values : ${operations[@]}"
